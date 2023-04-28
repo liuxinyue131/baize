3644fddadb59f93dfe462b9c9c574f8b212f2247
liuxinyue131 <131754830+liuxinyue131@users.noreply.github.com>
2023-04-28 14:56:41 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>AI 法律助手</title>
+        <title>白泽神兽</title>
         <meta
           name="description"
-          content="AI 法律助手"
+          content="白泽神兽"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -40,38 +40,6 @@ export default function Home() {
           <SearchDialog />
         </div>
 
-        <div className='mt-28 md:mt-40 text-center w-full'>
-          <h2 className='text-slate-500'>更多好玩</h2>
-          <ul className='grid grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 mt-4 max-w-lg mx-auto text-xs'>
-            <li>
-              <Link href="https://magickpen.com/?ref=lawcnai" className=' 
-              dark:bg-white/30
-              dark:text-slate-900
-              dark:border-slate-900
-               text-slate-400  border border-slate-300/30 transition-all hover:bg-white/50 hover:backdrop-blur-md py-2.5 rounded-md block'>
-                <Image src={'/MagickPen.png'}  width={100} height="20" className='w-full mb-1 h-5 object-contain' alt="MagickPen - 智能写作助手" />
-                智能写作助手
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.teach-anything.com/?ref=lawcnai" className='dark:bg-white/30
-              dark:border-slate-900
-              dark:text-slate-900 text-slate-400  border border-slate-300/30 transition-all hover:bg-white/50 hover:backdrop-blur-md py-2.5 rounded-md block'>
-                <Image src={'/TeachAnything.png'} width={100} height="20" className='w-full mb-1 h-5 object-contain' alt="Teach Anything - AI 百科全书" />
-                AI 百科全书
-              </Link>
-            </li>
-            <li>
-              <Link href="https://openl.io/?ref=lawcnai" className='dark:bg-white/30
-              dark:border-slate-900
-              dark:text-slate-900 text-slate-400  border border-slate-300/30 transition-all hover:bg-white/50 hover:backdrop-blur-md py-2.5 rounded-md block'>
-                <Image src={'/OpenL.png'} width={100} height="20" className='w-full mb-1 h-5 object-contain' alt="OpenL - AI 翻译专家" />
-                AI 翻译专家
-              </Link>
-            </li>
-          </ul>
-        </div>
-
         <div className="py-10 w-full md:flex items-center justify-center md:space-x-6">
           <div className="flex items-center justify-center space-x-6 mt-4 md:m-0">
             <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
@@ -79,7 +47,7 @@ export default function Home() {
                 href="https://github.com/lvwzhen/law-cn-ai"
                 className="flex items-center justify-center"
               >
-                <Image src={'/github.svg'} width="24" height="24" alt="Github logo" />
+            
               </Link>
             </div>
             <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
@@ -87,7 +55,6 @@ export default function Home() {
                 href="https://twitter.com/lvwzhen"
                 className="flex items-center justify-center"
               >
-                <Image src={'/twitter.svg'} width="24" height="24" alt="Twitter logo" />
               </Link>
             </div>
           </div>
