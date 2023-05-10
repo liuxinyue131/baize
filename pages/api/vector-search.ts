017c4ea6c6d46ee139230df1db66dc5e2164fe67
liuxinyue131 <131754830+liuxinyue131@users.noreply.github.com>
2023-05-10 14:04:46 +0800

    Update vector-search.ts

--- pages/api/vector-search.ts
+++ pages/api/vector-search.ts
@@ -10,6 +10,7 @@ export const config = {
   runtime: 'edge',
 }
 
+
 const openAiKey = process.env.OPENAI_KEY
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY
