49ff2518974dd96ec5e81ef01d44c02cf244f270
wusong <wusong@wusongdeMacBook-Pro.local>
2023-05-15 12:09:26 +0800

    logout

--- pages/api/vector-search.ts
+++ pages/api/vector-search.ts
@@ -64,7 +64,7 @@ export default async function handler(req: NextRequest) {
         input: sanitizedQuery,
       }),
     }).then((res) => res.json())
- //   console.log("moderationResponseString:"+JSON.stringify(moderationResponse))
+    console.log("moderationResponseString:"+JSON.stringify(moderationResponse))
 
 
     const [results] = moderationResponse.results
